0de144dda7f870911ef75fa01cfc744c7644c758
HiMatyu <144270275+MatveyTolstov@users.noreply.github.com>
2025-09-23 01:30:31 +0300

    добавил апишку

--- music_shop/music_shop/urls.py
+++ music_shop/music_shop/urls.py
@@ -19,8 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop_main.api import router as api_router
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("shop_main.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("shop_main.urls")),
+    path("api/", include(api_router.urls)),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
