66c55dff50d7a069bf1e42f3f0f070ff87e1a553
HiMatyu <144270275+MatveyTolstov@users.noreply.github.com>
2025-09-23 09:27:01 +0300

    поменял постгре на маму

--- music_shop/music_shop/settings.py
+++ music_shop/music_shop/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = "music_shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "music_shop",
+#         "USER": "postgres",
+#         "PASSWORD": "1",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "music_shop",
-        "USER": "postgres",
-        "PASSWORD": "1",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
